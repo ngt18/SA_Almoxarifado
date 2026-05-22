a909cf292e110ce866d6f772f08ee49264febcc4
Rafael Ruhoff Gonçalves casas <131457092+ngt18@users.noreply.github.com>
2026-05-21 21:02:35 -0300

    s
    s

--- backend/routes/equipamentos.js
+++ backend/routes/equipamentos.js
@@ -14,6 +14,19 @@ router.get("/listarTodos", (req, res) => {
   });
 });
 
+router.get("/listar/:id", (req, res) => {
+  const { id } = req.params;
+
+  const sql = "SELECT * FROM equipamentos WHERE id = ?"
+
+  db.query(sql, [id], (error, result) => {
+    if(error) {
+      return res.status(500).json(error);
+    }
+    res.status(200).json(result)
+  })
+})
+
 router.post("/criar", (req, res) => {
   const { nome, marca, patrimonio } = req.body;
 
@@ -26,6 +39,36 @@ router.post("/criar", (req, res) => {
 
     res.status(201).json({
       mensagem: "Equipamento cadastrado com sucesso!",
+    });
+  });
+});
+
+router.put("/editar/:id", (req, res) =>{
+  const {id} = req.params;
+  const {nome, marca, patrimonio} = req.body;
+
+  const sql = "UPDATE equipamentos SET nome = ?, marca = ?, patrimonio = ? WHERE id = ?";
+  db.query(sql, [nome, marca, patrimonio, id], (error) => {
+    if (error) {
+      return res.status(500).json(error);
+    }
+
+    res.status(200).json({
+      mensagem: "Equipamento atualizado com sucesso!",
+    });
+  });
+});
+
+
+router.delete("/excluir/:id", (req, res) => {
+  const { id } = req.params;
+  const sql = "DELETE FROM equipamentos WHERE id = ?";
+  db.query(sql, [id], (error) => {
+    if (error) {
+      return res.status(500).json(error);
+    }
+    res.status(200).json({
+      mensagem: "Equipamento excluído com sucesso!",
     });
   });
 });
